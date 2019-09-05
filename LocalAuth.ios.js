ea46591bc0596aa200dac83f9140ae823e60df38
Matthew <mattcoldbeans@gmail.com>
2019-09-05 10:34:20 -0500

    Update LocalAuth.ios.js
    Remove PasscodeStatus dependency for IOS

--- LocalAuth.ios.js
+++ LocalAuth.ios.js
@@ -7,7 +7,7 @@
 
 import { NativeModules } from 'react-native'
 import { createError } from './error'
-import PasscodeStatus from 'react-native-passcode-status'
+// import PasscodeStatus from 'react-native-passcode-status'
 
 let NativeLocalAuth = NativeModules.RNLocalAuth
 let LocalAuth = {
@@ -24,18 +24,18 @@ let LocalAuth = {
   },
 
   isDeviceSecure() {
-    if (!PasscodeStatus.supported) {
-      return Promise.reject(new Error('unable to determine'))
-    }
+//     if (!PasscodeStatus.supported) {
+     return Promise.reject(new Error('unable to determine'))
+//     }
 
-    return new Promise((resolve, reject) => {
-      PasscodeStatus.get(function (err, status) {
-        if (err) return reject(new Error(err))
-        if (status === 'unknown') return reject(new Error('unable to determine'))
+//     return new Promise((resolve, reject) => {
+//       PasscodeStatus.get(function (err, status) {
+//         if (err) return reject(new Error(err))
+//         if (status === 'unknown') return reject(new Error('unable to determine'))
 
-        resolve(status === 'enabled')
-      })
-    })
+//         resolve(status === 'enabled')
+//       })
+//     })
   },
 
   authenticate(opts) {
